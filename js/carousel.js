1fd528d2489d9f0b108f851118abe24771e61909
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-04-08 01:07:32 -0400

    complete 2nd page and 3rd page of work.

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,8 @@
 const siema1 = new Siema({
   selector: ".siema1",
-  duration: 400,
+  duration: 700,
   easing: "ease-in-out",
-  perPage: 2,
+  perPage: 1,
   startIndex: 0,
   draggable: true,
   multipleDrag: true,
@@ -11,3 +11,17 @@ const siema1 = new Siema({
 });
 document.querySelector(".prev1").addEventListener("click", () => siema1.prev());
 document.querySelector(".next1").addEventListener("click", () => siema1.next());
+
+const siema2 = new Siema({
+  selector: ".siema2",
+  duration: 700,
+  easing: "ease-in-out",
+  perPage: 1,
+  startIndex: 0,
+  draggable: true,
+  multipleDrag: true,
+  threshold: 20,
+  loop: true,
+});
+document.querySelector(".prev2").addEventListener("click", () => siema2.prev());
+document.querySelector(".next2").addEventListener("click", () => siema2.next());
