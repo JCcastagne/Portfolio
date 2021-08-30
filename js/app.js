1345b73fffe466d9cd25cda0ea615e9261723ac2
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2021-08-29 21:59:49 -0400

    fixed scroll snap issue on about page. remove un-needed scripts

--- js/app.js
+++ js/app.js
@@ -1,36 +1,15 @@
-document.addEventListener("DOMContentLoaded", init);
-
 function init() {
-  TweenMax.defaultEase = Linear.easeOut;
-
-  new fullpage("#fullpage", {
-    autoScrolling: true,
-    navigation: true,
-    keyboardScrolling: true,
-    animateAnchor: true,
-    navigationPosition: "left",
-    //Design
-    animateAnchor: true,
-    controlArrows: false,
-    responsiveSlides: true,
-    onLeave: (origin, destination, direction) => {
-      const section = destination.item;
-      const title = section.querySelector("h1");
-      const tl = new TimelineMax({ delay: 0.5 });
-      tl.fromTo(title, 0.5, { y: "50", opacity: 0 }, { y: "0", opacity: 1 });
-
-      if (destination.index === 1) {
-        const p = document.querySelector("p");
-
-        tl.fromTo(p, 0.7, { y: "100" }, { y: "0" });
-      }
-    },
-  });
+  let introCover = document.querySelector(".introCover");
+  console.log(introCover);
+  if (introCover) {
+    setTimeout(function () {
+      introCover.style.opacity = "0";
+    }, 1750);
+    setTimeout(function () {
+      introCover.style.display = "none";
+    }, 2760);
+    //2760 because animation of cover fading is 1000, adding an extra 10ms on second function just to make sure.
+    //cover is "position: absolute" which forces user to watch animation before scrolling.
+  }
 }
-
-// Code and design by Jean-Christophe Castagne
-// | https://github.com/JCcastagne/ |
-
-// Code is free to be used with proper credit, all media assets belong to the original owner and shall not be reused or redistributed.
-
-// fullpage.js is a library from https://github.com/alvarotrigo/fullPage.js/
+document.body.addEventListener("DOMContentLoaded", init());
