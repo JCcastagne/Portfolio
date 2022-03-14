972f1358fd3fb0fc59aa8b288a754a13160bf981
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2022-03-14 02:36:17 -0400

    Add styling for smaller screens on pages.
    Smaller screens also turn off Slides.js animations.

--- js/slidesView.js
+++ js/slidesView.js
@@ -1,4 +1,18 @@
 function init () {
+  //listener for screen sizes. Cancel "slides" animations if on mobile.
+  let animationsAreOn = true
+  window.addEventListener('resize', turnOffAnimations)
+
+  function turnOffAnimations () {
+    if (window.innerWidth <= 600) {
+      animationsAreOn = false
+      // console.log('turn OFF animations')
+    } else {
+      animationsAreOn = true
+      // console.log('turn ON animations')
+    }
+  }
+
   let sliders = document.querySelectorAll('.slides')
 
   sliders.forEach(slide => {
@@ -16,89 +30,91 @@ function init () {
     sliderImg3.addEventListener('click', () => selectImage(3))
 
     function selectImage (selectedImage) {
-      if (selectedImage === 1 && image1.position === 'left') {
-        // console.log('move image1 to middle')
-        //translate
-        image1.img.style.transform = 'translateX(50%) scale(1.5)'
-        image2.img.style.transform = 'translateX(-50%)'
-        image3.img.style.transform = 'translateX(0%)'
+      if (animationsAreOn) {
+        if (selectedImage === 1 && image1.position === 'left') {
+          // console.log('move image1 to middle')
+          //translate
+          image1.img.style.transform = 'translateX(50%) scale(1.5)'
+          image2.img.style.transform = 'translateX(-50%)'
+          image3.img.style.transform = 'translateX(0%)'
 
-        //z-index
-        image1.img.style.zIndex = '3'
-        image2.img.style.zIndex = '2'
-        image3.img.style.zIndex = '2'
+          //z-index
+          image1.img.style.zIndex = '3'
+          image2.img.style.zIndex = '2'
+          image3.img.style.zIndex = '2'
 
-        //border radius
-        image1.img.style.borderRadius = '13px'
-        image2.img.style.borderRadius = '11px'
-        image3.img.style.borderRadius = '11px'
+          //border radius
+          image1.img.style.borderRadius = '13px'
+          image2.img.style.borderRadius = '11px'
+          image3.img.style.borderRadius = '11px'
 
-        //update image positions
-        image1.position = 'center'
-        image2.position = 'left'
-        image3.position = 'right'
-      } else if ((selectedImage === 1 || 2) && image1.position === 'center') {
-        // console.log('move image1 to left, return image2 to center')
-        //translate
-        image1.img.style.transform = 'translateX(0%)'
-        image2.img.style.transform = 'translateX(0%) scale(1.5)'
-        image3.img.style.transform = 'translateX(0%)'
+          //update image positions
+          image1.position = 'center'
+          image2.position = 'left'
+          image3.position = 'right'
+        } else if ((selectedImage === 1 || 2) && image1.position === 'center') {
+          // console.log('move image1 to left, return image2 to center')
+          //translate
+          image1.img.style.transform = 'translateX(0%)'
+          image2.img.style.transform = 'translateX(0%) scale(1.5)'
+          image3.img.style.transform = 'translateX(0%)'
 
-        //z-index
-        image1.img.style.zIndex = '2'
-        image2.img.style.zIndex = '3'
-        image3.img.style.zIndex = '2'
+          //z-index
+          image1.img.style.zIndex = '2'
+          image2.img.style.zIndex = '3'
+          image3.img.style.zIndex = '2'
 
-        //border radius
-        image1.img.style.borderRadius = '11px'
-        image2.img.style.borderRadius = '13px'
-        image3.img.style.borderRadius = '11px'
+          //border radius
+          image1.img.style.borderRadius = '11px'
+          image2.img.style.borderRadius = '13px'
+          image3.img.style.borderRadius = '11px'
 
-        //update image positions
-        image1.position = 'left'
-        image2.position = 'center'
-        image3.position = 'right'
-      } else if (selectedImage === 3 && image3.position === 'right') {
-        // console.log('move image3 to center')
-        //translate
-        image1.img.style.transform = 'translateX(0%)'
-        image2.img.style.transform = 'translateX(50%)'
-        image3.img.style.transform = 'translateX(-50%) scale(1.5)'
+          //update image positions
+          image1.position = 'left'
+          image2.position = 'center'
+          image3.position = 'right'
+        } else if (selectedImage === 3 && image3.position === 'right') {
+          // console.log('move image3 to center')
+          //translate
+          image1.img.style.transform = 'translateX(0%)'
+          image2.img.style.transform = 'translateX(50%)'
+          image3.img.style.transform = 'translateX(-50%) scale(1.5)'
 
-        //z-index
-        image1.img.style.zIndex = '2'
-        image2.img.style.zIndex = '2'
-        image3.img.style.zIndex = '3'
+          //z-index
+          image1.img.style.zIndex = '2'
+          image2.img.style.zIndex = '2'
+          image3.img.style.zIndex = '3'
 
-        //border radius
-        image1.img.style.borderRadius = '11px'
-        image2.img.style.borderRadius = '11px'
-        image3.img.style.borderRadius = '13px'
+          //border radius
+          image1.img.style.borderRadius = '11px'
+          image2.img.style.borderRadius = '11px'
+          image3.img.style.borderRadius = '13px'
 
-        //update image positions
-        image1.position = 'left'
-        image2.position = 'right'
-        image3.position = 'center'
-      } else if ((selectedImage === 3 || 2) && image3.position === 'center') {
-        // console.log('move image3 to right, return image2 to center')
-        image1.img.style.transform = 'translateX(0%)'
-        image2.img.style.transform = 'translateX(0%) scale(1.5)'
-        image3.img.style.transform = 'translateX(0%)'
+          //update image positions
+          image1.position = 'left'
+          image2.position = 'right'
+          image3.position = 'center'
+        } else if ((selectedImage === 3 || 2) && image3.position === 'center') {
+          // console.log('move image3 to right, return image2 to center')
+          image1.img.style.transform = 'translateX(0%)'
+          image2.img.style.transform = 'translateX(0%) scale(1.5)'
+          image3.img.style.transform = 'translateX(0%)'
 
-        //z-index
-        image1.img.style.zIndex = '2'
-        image2.img.style.zIndex = '3'
-        image3.img.style.zIndex = '2'
+          //z-index
+          image1.img.style.zIndex = '2'
+          image2.img.style.zIndex = '3'
+          image3.img.style.zIndex = '2'
 
-        //border radius
-        image1.img.style.borderRadius = '11px'
-        image2.img.style.borderRadius = '13px'
-        image3.img.style.borderRadius = '11px'
+          //border radius
+          image1.img.style.borderRadius = '11px'
+          image2.img.style.borderRadius = '13px'
+          image3.img.style.borderRadius = '11px'
 
-        //update image positions
-        image1.position = 'left'
-        image2.position = 'center'
-        image3.position = 'right'
+          //update image positions
+          image1.position = 'left'
+          image2.position = 'center'
+          image3.position = 'right'
+        }
       }
     }
   })
