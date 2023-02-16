fbf8dcb56e5b7725b975d71579a27bd6bc72bcc4
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2023-02-15 22:57:50 -0500

    Complete Landing redesign

--- src/Pages/Home/Landing/Landing.js
+++ src/Pages/Home/Landing/Landing.js
@@ -1,36 +1,41 @@
 import './Landing.css'
 
-import name_hero from '../../../img/name_hero.svg'
-import lastName_hero from '../../../img/lastName_hero.svg'
 import { Link } from 'react-router-dom'
+
+import name_hero_light from '../../../img/name_hero_light.svg'
+import lastName_hero_light from '../../../img/lastName_hero_light.svg'
+import server_light from '../../../img/icons/server_light.svg'
+import laptop_light from '../../../img/icons/laptop_light.svg'
+import design_light from '../../../img/icons/design_light.svg'
 
 export default function Landing () {
   return (
-    <div class='Landing'>
-      <header>
-        <div className='name'>
-          <div className='firstName'>
-            <img src={name_hero} alt='Jean-Christophe' />
-          </div>
+    <header class='Landing'>
+      <div className='name'>
+        <img src={name_hero_light} alt='Jean-Christophe' />
+        <img src={lastName_hero_light} alt='Castagne' />
+      </div>
 
-          <div className='heroCard'></div>
-          <div className='heroCard'></div>
-
-          <div className='lastName'>
-            <img src={lastName_hero} alt='Castagne' />
-          </div>
+      <div className='skills'>
+        <div>
+          <img src={server_light} alt='Backend icon' />
+          <h1>Backend</h1>
         </div>
-        <div className='skills'>
-          <h1>Frontend & backend developer</h1>
-          <h2>UI & UX designer</h2>
-          <p>Web & mobile</p>
+        <span></span>
+        <div>
+          <img src={laptop_light} alt='Frontend icon' />
+          <h2>Frontend</h2>
         </div>
-      </header>
-      {/* <p>Developer & designer</p> */}
+        <span></span>
+        <div>
+          <img src={design_light} alt='Design icon' />
+          <h3>Design</h3>
+        </div>
+      </div>
       <p>
         Previously at <Link to='/work'>Applauz</Link> and{' '}
         <Link to='/work'>Vuzec</Link>
       </p>
-    </div>
+    </header>
   )
 }
