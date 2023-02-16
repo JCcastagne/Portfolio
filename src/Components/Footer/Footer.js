9d188efbe2af4ee8b0cd7aff6ee60c9c85aef9e5
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2023-02-16 10:13:11 -0500

    Complete Navbar redesign; colours updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,8 @@
 import './Footer.css'
 import { Link } from 'react-router-dom'
 
-import JC_logo_white from '../../img/JC_logo_white.svg'
-import star_white from '../../img/icons/star_white.svg'
-import arrow_top_right_white from '../../img/icons/arrow_top_right_white.svg'
+import JC_logo_light from '../../img/JC_logo_light.svg'
+import arrow_top_right_light from '../../img/icons/arrow_top_right_light.svg'
 
 export default function Footer () {
   return (
@@ -12,32 +11,32 @@ export default function Footer () {
         <section className='left' id='links'>
           <a href='mailto:jc.castagne96@gmail.com'>
             Email
-            <img src={arrow_top_right_white} alt='arrow top right' />
+            <img src={arrow_top_right_light} alt='arrow top right' />
           </a>
 
           <Link to='https://linkedin.com/in/jc-castagne/' target='_blank'>
             LinkedIn
-            <img src={arrow_top_right_white} alt='arrow top right' />
+            <img src={arrow_top_right_light} alt='arrow top right' />
           </Link>
           <Link to='https://instagram.com/jc.castagne/' target='_blank'>
             Instagram
-            <img src={arrow_top_right_white} alt='arrow top right' />
+            <img src={arrow_top_right_light} alt='arrow top right' />
           </Link>
           <Link to='https://github.com/JCcastagne' target='_blank'>
             Github
-            <img src={arrow_top_right_white} alt='arrow top right' />
+            <img src={arrow_top_right_light} alt='arrow top right' />
           </Link>
           <Link to='https://dribbble.com/jccastagne' target='_blank'>
             Dribbble
-            <img src={arrow_top_right_white} alt='arrow top right' />
+            <img src={arrow_top_right_light} alt='arrow top right' />
           </Link>
           <Link to='https://jccastagne.hashnode.dev/' target='_blank'>
             Blog
-            <img src={arrow_top_right_white} alt='arrow top right' />
+            <img src={arrow_top_right_light} alt='arrow top right' />
           </Link>
         </section>
         <section className='right' id='copyright'>
-          <img src={JC_logo_white} alt='jc logo' />
+          <img src={JC_logo_light} alt='jc logo' />
           <p>Jean-Christophe Castagne &copy; 2023</p>
         </section>
       </div>
