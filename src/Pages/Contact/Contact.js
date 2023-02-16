0a2ab22f6903a0cd9754ecd8c7101e1e8cc51b8a
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2023-02-16 11:34:10 -0500

    Completed Contact redesign.

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,31 +1,31 @@
 import './Contact.css'
 import { Link } from 'react-router-dom'
 
-import arrow_top_right from '../../img/icons/arrow_top_right.svg'
-import contact_hero from '../../img/contact_hero.svg'
+import arrow_top_right_light from '../../img/icons/arrow_top_right_light.svg'
+import contact_hero_colored from '../../img/contact_hero_colored.svg'
 
 export default function Contact () {
   return (
     <div class='Contact'>
-      <img src={contact_hero} alt='contact' className='contact_hero' />
+      <img src={contact_hero_colored} alt='contact' className='contact_hero' />
 
       <div className='details'>
         <section className='left' id='links'>
           <a href='mailto:jc.castagne96@gmail.com'>
             Email
-            <img src={arrow_top_right} alt='arrow top right' />
+            <img src={arrow_top_right_light} alt='arrow top right' />
           </a>
           <Link to='https://instagram.com/jc.castagne/' target='_blank'>
             Instagram
-            <img src={arrow_top_right} alt='arrow top right' />
+            <img src={arrow_top_right_light} alt='arrow top right' />
           </Link>
           <Link to='https://linkedin.com/in/jc-castagne/' target='_blank'>
             LinkedIn
-            <img src={arrow_top_right} alt='arrow top right' />
+            <img src={arrow_top_right_light} alt='arrow top right' />
           </Link>
           <Link to='https://www.fiverr.com/jccastagne' target='_blank'>
             Fiverr
-            <img src={arrow_top_right} alt='arrow top right' />
+            <img src={arrow_top_right_light} alt='arrow top right' />
           </Link>
         </section>
 
