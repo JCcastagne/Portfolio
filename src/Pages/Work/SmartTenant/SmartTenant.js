536a8b25086e7c193a067c9cc7e1561f5199e362
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2023-02-20 20:04:18 -0500

    Complete first .story section.

--- src/Pages/Work/SmartTenant/SmartTenant.js
+++ src/Pages/Work/SmartTenant/SmartTenant.js
@@ -11,7 +11,7 @@ import intelidevs_hero_light from '../../../img/portfolioAssets/SmartTenant/inte
 export default function SmartTenant () {
   return (
     <div class='Work' id='SmartTenant'>
-      <section className='intro'>
+      <section className='hero'>
         <header className='header'>
           <span>
             <img
@@ -38,116 +38,41 @@ export default function SmartTenant () {
         />
       </section>
 
-      {/* 
       <section className='story'>
-        <p className='title'>Problem discovery</p>
+        <p className='title'>Introduction</p>
+        <span className='heading'>
+          A mobile app built from the ground up{' '}
+          <b>offering tenants the ability to engage with their community</b>,
+          stay up to date with payments, earn rewards and enjoy a host of other
+          exciting features.
+        </span>
 
-        <div className='slide'>
-          <p>
-            Tenants are not engaged with their communities when renting a living
-            space.
-          </p>
-          <p>
-            While conducting early interviews with renters, many expressed
-            roadblocks when engaging with others in their rental property,
-            lacking a sense of connecting with the people around them.
-          </p>
-          <p>
-            Typically, they would simply avoid socializing with anyone and keep
-            to themselves.
-          </p>
-        </div>
-      </section>
-
-      <section className='story'>
-        <p className='title'>Our solution - SmartTenant</p>
-
-        <div className='slide'>
-          <p>
-            A social media platform that connects tenants in their community.
-          </p>
-          <img src={SmartTenant_1} alt='SmartTenant showcase banner' />
-          <caption>
-            Expand your social network by interacting with other tenants inside
-            the same building.
-          </caption>
-        </div>
-      </section>
-
-      <section className='story'>
-        <p className='title'>Staying connected with those around you</p>
-
-        <div className='slide'>
-          <p>- Post a status and pictures for others to engage with</p>
-          <p>
-            - Post announcements as property managers or community moderators
-          </p>
-          <p>- Get to know the people you live close with</p>
-          <img src={SmartTenant_1} alt='SmartTenant showcase banner' />
-          <caption>Newsfeed (left) and post/comments view (right)</caption>
-        </div>
-      </section>
-
-      <section className='story'>
-        <p className='title'>Stay in the know</p>
-
-        <div className='slide'>
-          <p>View your rental's amenities</p>
-          <p>Know who to contact</p>
-          <p>View other rental spaces</p>
-
-          <img src={SmartTenant_2} alt='SmartTenant showcase banner' />
-          <caption>
-            Building information(left), newsfeed (center) and post/comments view
-            (right)
-          </caption>
-        </div>
-      </section>
-
-      <section className='final'>
-        <div className='info'>
-          <p className='title'>Final outcomes</p>
-          <p>
-            By the end of the project’s term, Algonquin College approached our
-            team to take over the application under their name, furthering their
-            relationship with the parent company; Smart Living Properties.
-          </p>
-          <div className='grid'>
-            <div>
-              <p className='subTitle'>Design</p>
-              <p>To be completed</p>
-              <p>To be completed</p>
-              <p>To be completed</p>
-              <p>To be completed</p>
-            </div>
-            <div>
-              <p className='subTitle'>Frontend</p>
-              <p>To be completed</p>
-              <p>To be completed</p>
-              <p>To be completed</p>
-            </div>
-            <div>
-              <p className='subTitle'>Backend</p>
-              <p>To be completed</p>
-              <p>To be completed</p>
-            </div>
+        <div className='textGrid'>
+          <div>
+            <p className='title'>My role</p>
+            <p>Lead UI/UX Designer and Full Stack Developer:</p>
+            <p>
+              Overseeing market research, information architecture, user flows,
+              wireframes, interactive prototypes, and usability testing.
+            </p>
+            <p>
+              Contribute to the implementation of robust and scalable solutions
+              that meet user requirements through coding, testing, and
+              debugging.
+            </p>
           </div>
-          <div className='pictureGrid'>
-            <img src={SmartTenant_1} alt='landing home' className='bgBlur' />
-            <img src={SmartTenant_1} alt='landing home' />
-            <div className='row'>
-              <div className='col'>
-                <img src={SmartTenant_2} alt='trading home' />
-                <img src={SmartTenant_3} alt='dashboard transactions' />
-              </div>
-              <div className='col'>
-                <img src={SmartTenant_3} alt='dashboard balance' />
-                <img src={SmartTenant_2} alt='dashboard balance' />
-              </div>
-            </div>
+          <div>
+            <p className='title'>Tools</p>
+            <p>
+              Figma, React Native, NodeJS, Javascript, Google Firebase/Firestore
+            </p>
+          </div>
+          <div>
+            <p className='title'>Team</p>
+            <p>InteliDevs' team; myself and 4 Full Stack Developers.</p>
           </div>
         </div>
-      </section> */}
+      </section>
     </div>
   )
 }
