65a9ab6aa5d04012ca12d215c1644a34e65339dc
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2023-02-22 18:34:48 -0500

    Scroll to top when going to root page.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,8 +3,12 @@ import Navbar from '../../Components/Navbar/Navbar'
 import Landing from './Landing/Landing'
 import Projects from './Projects/Projects'
 import Footer from '../../Components/Footer/Footer'
+import { useEffect } from 'react'
 
 export default function Home () {
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
   return (
     <div class='Home'>
       <div className='container'>
