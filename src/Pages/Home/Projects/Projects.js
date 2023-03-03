6a0c311bc876f1e2976c4975d8941a5ad4efd94d
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2023-03-03 12:58:44 -0500

    Wrap cards with Atropos, update styling to remove conflicts.

--- src/Pages/Home/Projects/Projects.js
+++ src/Pages/Home/Projects/Projects.js
@@ -1,63 +1,74 @@
 import { Link } from 'react-router-dom'
+import Atropos from 'atropos/react'
 
 import './Projects.css'
+import 'atropos/css'
+
 import arrow_top_right from '../../../img/icons/arrow_top_right.svg'
 import arrow_top_right_light from '../../../img/icons/arrow_top_right_light.svg'
 
 export default function Projects () {
   return (
     <div class='Projects'>
-      <Link to='work/vuzec' className='project' id='Vuzec'>
-        <div className='title'>
-          <p>Vuzec</p>
-          <img src={arrow_top_right_light} alt='arrow top right' />
-        </div>
-        <div class='chips'>
-          <p>Dev</p>
-          <p>Design</p>
-          <p>App</p>
-        </div>
-        <p>Web3 asset trading platform</p>
-      </Link>
+      <Atropos activeOffset={40} shadowScale={1.05} className='hoverContainer'>
+        <Link to='work/vuzec' className='project' id='Vuzec'>
+          <div className='title'>
+            <p>Vuzec</p>
+            <img src={arrow_top_right_light} alt='arrow top right' />
+          </div>
+          <div class='chips'>
+            <p>Dev</p>
+            <p>Design</p>
+            <p>App</p>
+          </div>
+          <p>Web3 asset trading platform</p>
+        </Link>
+      </Atropos>
 
-      <Link to='work/applauz' className='project' id='Applauz'>
-        <div className='title'>
-          <p>Applauz (NDA)</p>
-          <img src={arrow_top_right_light} alt='arrow top right' />
-        </div>
-        <div class='chips'>
-          <p>Dev</p>
-          <p>App</p>
-          <p>Web</p>
-        </div>
-        <p>Social HR tool</p>
-      </Link>
+      <Atropos activeOffset={40} shadowScale={1.05} className='hoverContainer'>
+        <Link to='work/applauz' className='project' id='Applauz'>
+          <div className='title'>
+            <p>Applauz (NDA)</p>
+            <img src={arrow_top_right_light} alt='arrow top right' />
+          </div>
+          <div class='chips'>
+            <p>Dev</p>
+            <p>App</p>
+            <p>Web</p>
+          </div>
+          <p>Social HR tool</p>
+        </Link>{' '}
+      </Atropos>
 
-      <Link to='work/fiverr' className='project' id='Fiverr'>
-        <div className='title'>
-          <p>Fiverr</p>
-          <img src={arrow_top_right_light} alt='arrow top right' />
-        </div>
-        <div class='chips'>
-          <p>Design</p>
-          <p>App</p>
-          <p>App</p>
-        </div>
-        <p>Freelance UI/UX design</p>
-      </Link>
+      <Atropos activeOffset={40} shadowScale={1.05} className='hoverContainer'>
+        <Link to='work/fiverr' className='project' id='Fiverr'>
+          <div className='title'>
+            <p>Fiverr</p>
+            <img src={arrow_top_right_light} alt='arrow top right' />
+          </div>
+          <div class='chips'>
+            <p>Design</p>
+            <p>App</p>
+            <p>App</p>
+          </div>
+          <p>Freelance UI/UX design</p>
+        </Link>{' '}
+      </Atropos>
 
-      <Link to='work/smarttenant' className='project' id='SmartTenant'>
-        <div className='title'>
-          <p>SmartTenant</p>
-          <img src={arrow_top_right} alt='arrow top right' />
-        </div>
-        <div class='chips'>
-          <p>App</p>
-          <p>Design</p>
-          <p>Dev</p>
-        </div>
-        <p>Social property management</p>
-      </Link>
+      <Atropos activeOffset={40} shadowScale={1.05} className='hoverContainer'>
+        <Link to='work/smarttenant' className='project' id='SmartTenant'>
+          <div className='title'>
+            <p>SmartTenant</p>
+            <img src={arrow_top_right} alt='arrow top right' />
+          </div>
+          <div class='chips'>
+            <p>App</p>
+            <p>Design</p>
+            <p>Dev</p>
+          </div>
+          <p>Social property management</p>
+        </Link>{' '}
+      </Atropos>
     </div>
   )
 }
