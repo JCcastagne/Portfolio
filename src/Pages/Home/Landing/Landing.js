0272b2ce55703787380f28f897e6d875471ee89b
JC <jc@JC-M2Pro.local>
2023-03-07 14:22:49 -0500

    Remove unused code: dynamic js card styling is replaced by Atropos.

--- src/Pages/Home/Landing/Landing.js
+++ src/Pages/Home/Landing/Landing.js
@@ -7,38 +7,14 @@ import lastName_hero_light from '../../../img/lastName_hero_light.svg'
 import server_light from '../../../img/icons/server_light.svg'
 import laptop_light from '../../../img/icons/laptop_light.svg'
 import design_light from '../../../img/icons/design_light.svg'
-import { useEffect, useState } from 'react'
+
 
 export default function Landing () {
-  const [scaleLanding, setScaleLanding] = useState('100%')
 
-  // window.addEventListener('scroll', handleScroll)
-
-  // function handleScroll (e) {
-  //   let scroll = window.scrollY
-  //   let height = window.innerHeight
-
-  //   let percentOfWindowHeight = Math.floor((scroll * 100) / height)
-
-  //   // percentOfWindowHeight = percentage of where the user is scrolled at compared to the full window height
-  //   // we subtract percentOfWindowHeight from 100 because we want to flip the number so that make it progressively smaller
-  //   // then we use Math.max to choose between two numbers, whichever is biggest between the product of percentOfWindowHeight and 66;
-  //   // 66 being the smallest we want the scale to be
-
-  //   setScaleLanding(Math.max(100 - percentOfWindowHeight, 66))
-  // }
-
-  // useEffect(() => {
-  //   console.log(scaleLanding)
-  // })
 
   return (
     <header
       className='Landing'
-      style={{
-        transform: `scale(${scaleLanding}%)`,
-        opacity: `${scaleLanding}%`
-      }}
     >
       <div className='name'>
         <img src={name_hero_light} alt='Jean-Christophe' />
